1f895e878dc459e5be6a96a1d2241f49b593bb86
TheMWA <carterjay.cad@gmail.com>
2016-11-02 20:09:18 -0500

    Add files via upload
    tttt

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+public static void main(String[] args) {
+int sum=0;
+System.out.println("Hello, World");
+System.out.println("wow!");
+String somevariable = "thisissomevariable";
+System.out.println("somevariable");
+for (int j = 1; j <= 1000; j=j+5){
+sum += j;
+System.out.println("again?");}
+if (somevariable == "x"){
+	System.out.println("true");}
+else {
+System.out.println("false");}
+		
+	
+}
+}
